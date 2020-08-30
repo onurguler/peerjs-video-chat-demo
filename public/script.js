b689853ba9b37ae33634f5fd9aef5ef1ae7f91ce
Onur Osman Güler <guleronurosman@gmail.com>
2020-08-30 18:44:58 +0300

    Display current user's video

--- public/script.js
+++ public/script.js
@@ -1,7 +1,33 @@
 const socket = io('/');
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: 3001
+});
 
-socket.emit('join-room', ROOM_ID, 10);
+const videoGrid = document.getElementById('video-grid');
 
-socket.on('user-connected', userId => {
-  console.log('User connected: ' + userId);
-})
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream);
+
+  socket.on('user-connected', userId => {
+    console.log('User connected: ' + userId);
+  });
+});
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
