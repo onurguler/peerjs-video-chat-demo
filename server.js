b2b10bca4f7b8271fd4d051cc959179e152906bd
Onur Osman Güler <guleronurosman@gmail.com>
2020-08-30 18:17:16 +0300

    Generate random uuid and redirect room

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const { v4: uuid4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuid4()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
 
 const PORT = process.env.PORT || 3000;
 
